6239a09cc0faf997e96db85ac48b4986fd7b09d8
winderica <winderica@gmail.com>
2021-05-11 16:55:12 +0800

    Fix: fix style conflicts when the target page also uses material ui

--- src/views/content/index.tsx
+++ src/views/content/index.tsx
@@ -1,4 +1,5 @@
 import { StyledEngineProvider, ThemeProvider } from '@material-ui/core';
+import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
 import React, { FC, StrictMode } from 'react';
 
 import { Widget } from './Widget';
@@ -7,17 +8,23 @@ import { PortContext } from '@contexts/port';
 import { usePort } from '@hooks/usePort';
 import { useTheme } from '@styles/theme';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'iPot-jss-',
+});
+
 export const Content: FC = () => {
   const theme = useTheme();
   const port = usePort();
   return (
     <StrictMode>
       <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <PortContext.Provider value={port}>
-            <Widget />
-          </PortContext.Provider>
-        </ThemeProvider>
+        <StylesProvider generateClassName={generateClassName}>
+          <ThemeProvider theme={theme}>
+            <PortContext.Provider value={port}>
+              <Widget />
+            </PortContext.Provider>
+          </ThemeProvider>
+        </StylesProvider>
       </StyledEngineProvider>
     </StrictMode>
   );
